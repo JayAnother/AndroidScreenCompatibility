87906e44d3cd1801971f7df9a21cd86e6c61bc36
Jay <jay.wang@imaginato.com>
2018-10-17 17:57:13 +0800

    Native RTL support in Android 4.2

---
+++ app/src/main/java/com/jay/androidscreencompatibility/ui/layout/LayoutDirectionActivity.java
@@ -0,0 +1,53 @@
+package com.jay.androidscreencompatibility.ui.layout;
+
+import com.jay.androidscreencompatibility.R;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LayoutDirectionActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_direction)
+    TextView tvDirection;
+
+    @BindView(R.id.rl_direction_left_right)
+    RelativeLayout rlDirectionLeftRight;
+
+    @BindView(R.id.rl_direction_start_end)
+    RelativeLayout rlDirectionStartEnd;
+
+    boolean isLeftRight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_direction);
+        ButterKnife.bind(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @OnClick(R.id.btn_exchange)
+    public void onViewClicked() {
+        if (isLeftRight) {
+            tvDirection.setText("Current Direction is Left-To-Right");
+            tvDirection.setTextDirection(View.TEXT_DIRECTION_LTR);
+            rlDirectionLeftRight.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
+            rlDirectionStartEnd.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
+        }else{
+            tvDirection.setText("Current Direction is Right-To-Left");
+            tvDirection.setTextDirection(View.TEXT_DIRECTION_RTL);
+            rlDirectionLeftRight.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+            rlDirectionStartEnd.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        }
+        isLeftRight = !isLeftRight;
+    }
+}
